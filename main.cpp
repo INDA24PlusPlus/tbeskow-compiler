3fdd7f5ed0aa7666e193df64d9aeb1a4fc35ea89
TheodorBeskow <theodor.beskow@icloud.com>
2024-10-08 23:43:28 +0200

    Now runs fibonacci from AST

--- main.cpp
+++ main.cpp
@@ -24,7 +24,9 @@ typedef vector<vl> vvl;
 typedef vector<vpii> vvpii;
 typedef vector<vpl> vvpl;
 
-enum Type {
+map<string, ll> variables;
+
+enum Type{
     Start,
     While,
     If,
@@ -36,10 +38,17 @@ enum Type {
     Number,
 };
 
-
+enum Operator{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+};
 
 struct AST{
     Type type;
+    Operator op;
+    ll value;
     string variable, compare;
     AST *expression1, *expression2;
     vector<AST*> body;
@@ -53,9 +62,54 @@ struct AST{
             cin >> in; 
             expression2 = new AST(Type::Expression);
             expression2->construct(in);
+            if(compare == ">" || compare == ">="){
+                compare[0] = '<';
+                swap(expression1, expression2);
+            }
+        }
+        if(type == Type::Expression){
+            for(int i = ((ll)in.size())-1;i>=0;i--){
+                if(in[i] == '+' || in[i] == '-'){
+                    op = (in[i] == '+') ? Operator::Addition : Operator::Subtraction;
+                    expression1 = new AST(Type::Expression);
+                    expression1->construct(in.substr(0, i));
+                    expression2 = new AST(Type::Expression);
+                    expression2->construct(in.substr(i+1));
+                    return;
+                }
+            }
+            for(int i = ((ll)in.size())-1;i>=0;i--){
+                if(in[i] == '*' || in[i] == '/'){
+                    op = (in[i] == '*') ? Operator::Multiplication : Operator::Division;
+                    expression1 = new AST(Type::Expression);
+                    expression1->construct(in.substr(0, i));
+                    expression2 = new AST(Type::Expression);
+                    expression2->construct(in.substr(i+1));
+                    return;
+                }
+            }
+            try{
+                value = stoi(in);
+                type = Type::Number;
+            }catch(exception e){
+                type = Type::Variable;
+                variable = in;
+            }
+            return;
+        }
+        if(type == Type::Assignment){
+            cin >> in;
+            expression1 = new AST(Type::Expression);
+            expression1->construct(in);
+            return;
+        }
+        if(type == Type::Print){
+            cin >> in;
+            expression1 = new AST(Type::Expression);
+            expression1->construct(in);
+            return;
         }
         while(cin >> in){
-            deb(in);
             if(in == "{") continue;
             if(in == "}") break;
 
@@ -70,26 +124,63 @@ struct AST{
             }else if(in == "int"){
                 AST *ast = new AST(Type::Assignment);
                 string equals;
-                // cin >> ast->variable >> equals >> ast->expression1;
+                cin >> ast->variable >> equals;
+                ast->construct();
+                body.pb(ast);
+            }else if(in == "print"){
+                AST *ast = new AST(Type::Print);
                 ast->construct();
                 body.pb(ast);
             }
         }
     }
 
-
     ll operator()(){
+        if(type == Type::Start) return 1;
+        if(type == Type::Number) return value;
+        if(type == Type::Variable) return variables[variable];
+        ll a = expression1->operator()();
+        ll b = expression2->operator()();
+        if(type == Type::Expression){
+            if(op == Operator::Addition) return a+b;
+            if(op == Operator::Subtraction) return a-b;
+            if(op == Operator::Multiplication) return a*b;
+            if(op == Operator::Division) return a/b;
+        }
+        if(type == Type::While || type == Type::If){
+            if(compare == "<") return a < b;
+            if(compare == "<=") return a <= b;
+            if(compare == "==") return a == b;
+            if(compare == "!=") return a != b;
+        }
+        return 1;
+    }
 
-        return true;
+    void run(){
+        if(type == Type::While || type == Type::If || type == Type::Start){
+            while(operator()()){
+                for(auto ast : body){
+                    ast->run();
+                }
+                if(type != Type::While) break;
+            }
+            return;
+        }
+        if(type == Type::Print) cout << expression1->operator()() << endl;
+        if(type == Type::Assignment){
+            ll val = expression1->operator()();
+            variables[variable] = val;
+        }
     }
 };
 
 int main() {
 
-    freopen("code.txt", "r", stdin);
+    freopen("fib.txt", "r", stdin);
 
     AST ast(Type::Start);
     ast.construct();
+    ast.run();
 
     return 0;
 }
